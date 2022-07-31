6e78b5096bc36ade0b8ad6ebf0fab110753c906e
DESKTOP-F1FBGCO\asus <laisasantika86@gmail.com>
2022-07-31 22:20:55 +0700

    tambah api login

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -8,4 +8,51 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+    public function register(Request $request){
+        $validateData = $request->validate([
+            'name'=> 'required|max:25',
+            'email' => 'email| required | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        //create user
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=> $request->bcrypt($request->password),
+        ]);
+
+        $user->save;
+
+        return response()->json($user, 201);
+    }
+    
+    public function login(Request $request){
+        $validateData = $request->validate([
+            'email' => 'email| required | unique:users',
+            'password' => 'required | confirmed',
+        ]);
+
+        $login_detail = request(['email','password']);
+
+        if(!Auth::attempt($login_detail)){
+            return response()->json([
+                'eror' => 'Login gagal. cek kembali email dan password anda !!'
+            ], 401);
+            
+        }
+
+        $user = $request->user();
+        $tokenResult =$user->createToken('AccessToken');
+        $token = $tokenResult->token;
+        $token->save;
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_id' => $token->id,
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('register','API\Auth\AuthController@register');
+Route::post('login','API\Auth\AuthController@login');
